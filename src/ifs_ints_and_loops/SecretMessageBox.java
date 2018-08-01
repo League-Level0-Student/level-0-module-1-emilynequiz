503d142d4b44187429b03f9c9dd4523018ba8a52
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-07-31 17:55:42 -0700

    riddler, secret message box

--- src/ifs_ints_and_loops/SecretMessageBox.java
+++ src/ifs_ints_and_loops/SecretMessageBox.java
@@ -1,32 +1,40 @@
 package ifs_ints_and_loops;
 //    Copyright (c) The League of Amazing Programmers 2013-2017
-//    Level 0
 
+//    Level 0
 
 import javax.swing.JOptionPane;
 
 /**
  * Secret Message Box / Secure Messaging System
  * 
- * You want to leave a message on one of the Mac computers so that only your friend can read it.
- * You set up the passcode and the secret message. 
- * Your friend types in the passcode to retrieve the message.
+ * You want to leave a message on one of the Mac computers so that only your
+ * friend can read it. You set up the passcode and the secret message. Your
+ * friend types in the passcode to retrieve the message.
  * 
  */
 
 public class SecretMessageBox {
 
 	// 0. Make a main method and put steps 1-5 inside it
-	
-	// 1. Set a password in a String variable
-	
-	// 2. Using a pop-up, ask the first person for a secret message and store it in a variable
+	public static void main(String[] args) {
 
-	// 3. Now use a pop-up to tell the NEXT user that they can only see the secret message 
-	//    if they can guess the passcode
+		// 1. Set a password in a String variable
+		String Password = "hello";
+		// 2. Using a pop-up, ask the first person for a secret message and store it in
+		// a variable
+		String answer = JOptionPane.showInputDialog("Tell me a secret message");
+		// 3. Now use a pop-up to tell the NEXT user that they can only see the secret
+		// message
+		// if they can guess the passcode
+		String guess = JOptionPane.showInputDialog("you can only see the secret if you can guess the passcode");
+		// 4. If their guess matches the password, show them the secret message
+		if (guess.equals("passcode")) {
+			JOptionPane.showMessageDialog(null, answer);
 
-	// 4. If their guess matches the password, show them the secret message
-
-	// 5. If the password does not match, pop-up "INTRUDER!!"
-
+			// 5. If the password does not match, pop-up "INTRUDER!!"
+		} else {
+			JOptionPane.showMessageDialog(null, "INTRUDER!");
+		}
+	}
 }
--- src/ifs_ints_and_loops/TheRiddler.java
+++ src/ifs_ints_and_loops/TheRiddler.java
@@ -2,24 +2,38 @@ package ifs_ints_and_loops;
 //    Copyright (c) The League of Amazing Programmers 2013-2017
 //    Level 0
 
-
+import javax.swing.JOptionPane;
 
 public class TheRiddler {
 
 	public static void main(String[] args) {
 
 		// 1. Make a variable to hold the score
-
+int score = 0;
 		// 3. Ask the user a riddle. Here are some ideas: bit.ly/some-riddles
-
+String answer = JOptionPane.showInputDialog("I'm tall whem I'm young and I'm short when I'm old.What am I?"); 
+	
 		// 4. If they got the answer right, pop up "correct!" and increase the score by one
-
+if(answer.equalsIgnoreCase("Candle")) {
+	JOptionPane.showMessageDialog(null,"Correct!");
+	score = score +1;
+	
+}else {
 		// 5. Otherwise, say "wrong" and tell them the answer
-
+JOptionPane.showMessageDialog(null, "Wrong!");
+}
 		// 6. Add some more riddles
-
+answer = JOptionPane.showInputDialog("What has hands but can not clap?"); 
 		// 2. Make a pop up to show the score.
-		
+score = score +1;
+if(answer.equalsIgnoreCase("Clock")) {
+	
+}else {
+
+}else {
+	JOptionPane.showMessageDialog(null, "Wrong!");
+}
+JOptionPane.showMessageDialog(null, "Your score is " + score);
 	}
 }
 
